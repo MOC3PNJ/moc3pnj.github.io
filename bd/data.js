8fa8aee7ab78101b2cb3451100149581d2a0faad
MOC3PNJ <m.pnj@bk.ru>
2025-07-02 23:12:14 -0400

    Update data.js

--- bd/data.js
+++ bd/data.js
@@ -35,6 +35,42 @@ const peliculas = [
     portada: "https://ejemplo.com/portada-interestelar.jpg",
     link: "https://ejemplo.com/interestelar"
   }
+  {
+    id: "pelicula-001",
+    nombre: "El Padrino",
+    año: 1972,
+    categoria: "Drama",
+    tipo: "Película",
+    portada: "https://ejemplo.com/portada-el-padrino.jpg",
+    link: "https://ejemplo.com/el-padrino"
+  },
+  {
+    id: "pelicula-002",
+    nombre: "Pulp Fiction",
+    año: 1994,
+    categoria: "Crimen",
+    tipo: "Película",
+    portada: "https://ejemplo.com/portada-pulp-fiction.jpg",
+    link: "https://ejemplo.com/pulp-fiction"
+  },
+  {
+    id: "serie-001",
+    nombre: "Stranger Things",
+    año: 2016,
+    categoria: "Ciencia Ficción",
+    tipo: "Serie",
+    portada: "https://ejemplo.com/portada-stranger-things.jpg",
+    link: "https://ejemplo.com/stranger-things"
+  },
+  {
+    id: "pelicula-003",
+    nombre: "Interestelar",
+    año: 2014,
+    categoria: "Ciencia Ficción",
+    tipo: "Película",
+    portada: "https://ejemplo.com/portada-interestelar.jpg",
+    link: "https://ejemplo.com/interestelar"
+  }
 ];
 
 export { peliculas };
