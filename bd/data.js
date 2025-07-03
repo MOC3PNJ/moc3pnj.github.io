869e7e72f47db1b3bc13d7f5ed3ba9bdc360fa5d
MOC3PNJ <m.pnj@bk.ru>
2025-07-02 22:03:35 -0400

    Create data.js

---
+++ bd/data.js
@@ -0,0 +1,33 @@
+// data.js
+const peliculas = [
+  {
+    nombre: "El Padrino",
+    año: 1972,
+    categoria: "Drama",
+    tipo: "Película",
+    link: "https://ejemplo.com/el-padrino"
+  },
+  {
+    nombre: "Pulp Fiction",
+    año: 1994,
+    categoria: "Crimen",
+    tipo: "Película",
+    link: "https://ejemplo.com/pulp-fiction"
+  },
+  {
+    nombre: "Stranger Things",
+    año: 2016,
+    categoria: "Ciencia Ficción",
+    tipo: "Serie",
+    link: "https://ejemplo.com/stranger-things"
+  },
+  {
+    nombre: "Interestelar",
+    año: 2014,
+    categoria: "Ciencia Ficción",
+    tipo: "Película",
+    link: "https://ejemplo.com/interestelar"
+  }
+];
+
+export { peliculas };
